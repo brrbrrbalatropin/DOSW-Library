b332222deaea9a7e2c597908fd48d7c9b76bb614
Daniel Felipe Rayo Rodriguez <daniel.rayo@mail.escuelaing.edu.co>
2026-04-09 00:54:40 -0500

    :cat: intento arreglar la pipeline

--- src/test/java/edu/eci/dosw/DoswLibraryApplicationTests.java
+++ src/test/java/edu/eci/dosw/DoswLibraryApplicationTests.java
@@ -3,8 +3,10 @@ package edu.eci.dosw;
 import edu.eci.dosw.DOSW_Library.DoswLibraryApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = DoswLibraryApplication.class)
+@ActiveProfiles("test")
 class DoswLibraryApplicationTests {
 
 	@Test
